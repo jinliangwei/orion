288c279f9f47564363b3499b6cc90e9450b08e3c
Jinliang Wei <jinliangwei1@gmail.com>
2017-02-22 21:37:36 -0500

    updates

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,13 +1,23 @@
 #!/usr/bin/env bash
 
-sudo apt-get install g++-5 libnuma-dev libblas-dev liblapack-dev libatlas-base-dev -y
-ls /usr/lib
+sudo apt-get update
+sudo apt-get --ignore-missing -y install \
+    git \
+    g++-5 \
+    uuid-dev \
+    libnuma-dev \
+    valgrind \
+    libblas-dev \
+    liblapack-dev \
+    libatlas-base-dev \
+    libgflags-dev \
+    libgoogle-glog-dev \
+    libsnappy-dev \
+    libgtest-dev \
+    google-perftools \
+    julia
 
 if [ ! -f /usr/lib/libcblas.so ]; then
     echo "link libcblas!!"
     sudo ln -s /usr/lib/libcblas.so.3 /usr/lib/libcblas.so
 fi
-
-git clone git@github.com:jinliangwei/third_party.git
-
-cd third_party; make third_party_core; make eigen
