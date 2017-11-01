20f05d97286b79e81c0df2a3c4e1107286416a93
jinliangwei <jinliangwei1@gmail.com>
2017-11-01 19:14:29 +0000

    added lr

--- apps/lr/orion_lr.jl
+++ apps/lr/orion_lr.jl
@@ -1,8 +1,9 @@
 include("/home/ubuntu/orion/src/julia/orion.jl")
 
-const K = 100
+const data_path = "/home/ubuntu/data/libsvm/a1a"
 const num_iterations = 10
-step_size = 0.001
+const step_size = 0.001
+Orion.@share const num_features = 123
 
 # set path to the C++ runtime library
 Orion.set_lib_path("/home/ubuntu/orion/lib/liborion_driver.so")
@@ -10,38 +11,53 @@ Orion.load_constants()
 # test library path
 Orion.helloworld()
 
- = Orion.DistArray{Float32}()
-W = Orion.DistArray{Float32}()
-H = Orion.DistArray{Float32}()
-ratings.num_dims = 2
-W.id = 1
-H.id = 2
-W.num_dims = 2
-H.num_dims = 2
-ratings.dims = [6000, 4000]
-W.dims = [6000, 100]
-H.dims = [4000, 100]
-
-Orion.@transform for i = 1:num_iterations
-    Orion.@parallel_for for rating in ratings
-	x_idx = rating[1][1]
-	y_idx = rating[1][2]
-	rv = rating[2]
-
-        if rv > 0
-            x_idx += 1
-        end
-
-        W_row = W[x_idx, :]
-	H_row = H[y_idx, :]
-	pred = dot(W_row, H_row)
-	diff = rv - pred
-	W_grad = -2 * diff .* H_row
-	H_grad = -2 * diff .* W_row
-	W[x_idx, :] = W_row - step_size .* W_grad
-	H[y_idx, :] = H_row - step_size .*H_grad .* K
+Orion.@share function parse_line(index::Int64, line::AbstractString)
+    feature_vec = Vector{Tuple{Int64, Float32}}()
+    tokens = split(line, ' ')
+    label = parse(Int64, tokens[1])
+    push!(feature_vec, (index * num_features, label))
+    for token in tokens[2:end]
+        feature = split(token, ":")
+        feature_id = parse(Int64, feature[1]) + 1
+        feature_val = parse(Float32, feature[2])
+        push!(feature_vec, (feature_id, feature_val))
     end
-    step_size = step_size * 0.9
+    return feature_vec
 end
 
-#ratings = Orion.text_file(data_path, parse_line)
+data_str = Orion.text_file(data_path)
+indexed_data_str = Orion.zip_with_index(data_str)
+features_mat = Orion.map(indexed_data_str, parse_line)
+Orion.materialize(features_mat)
+
+weights = Orion.rand(num_features)
+Orion.materialize(weights)
+
+Orion.@share function sigmoid(z)
+    return 1.0 ./ (1.0 .+ exp(-z))
+end
+
+Orion.set_access_granularity(features, (1, :))
+
+weights_buf = Orion.zeros(num_features)
+Orion.materialize(weights_buf)
+
+for i = 1:num_iterations
+    Orion.@parallel_for for feature in features
+        sum = 0.0
+        label = feature[1][2]
+        for feature_pair in feature[2:end]
+            fid = feature_pair[1]
+            fval = feature_pair[2]
+            sum += weights[fid] * fval
+        end
+        error = label - sigmoid(sum)
+        for feature_pair in feature[2:end]
+            fid = feature_pair[1]
+            fval = feature_pair[2]
+            weights_buf[fid] -= step_size * fval * error
+        end
+    end
+    Orion.add_to(weights, weights_buf)
+    Orion.zeros(weights_buf)
+end
